ff1d25452a0c77e14d7b6130383dd3029b42ee50
Artur Angiel <arturangiel@gmail.com>
2022-09-28 14:45:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ data "oci_core_image_shape" "amd" {
 
 data "oci_identity_availability_domain" "ad2" {
   compartment_id = var.compartment_ocid
-  ad_number      = 2
+  id = "iAGY:EU-FRANKFURT-1-AD-2"
 }
 
 
