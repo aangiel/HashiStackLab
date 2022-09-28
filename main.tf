8982ff64c5cd90ba1523eb844f3ad9fadc0ccfab
Artur Angiel <arturangiel@gmail.com>
2022-09-28 13:56:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,19 +24,30 @@ resource "oci_core_subnet" "hashistack" {
 }
 
 
-resource "oci_core_shape_management" "arm" {
-  # availability_domain = data.oci_identity_availability_domain.ad1.name
-  compartment_id = var.compartment_ocid
-  shape_name     = "VM.Standard.A1.Flex"
-  image_id       = var.image_ocid
+# resource "oci_core_shape_management" "arm" {
+#   # availability_domain = data.oci_identity_availability_domain.ad1.name
+#   compartment_id = var.compartment_ocid
+#   shape_name     = "VM.Standard.A1.Flex"
+#   image_id       = var.image_ocid
+# }
+
+# resource "oci_core_shape_management" "amd64" {
+#   # availability_domain = data.oci_identity_availability_domain.ad1.name
+#   compartment_id = var.compartment_ocid
+#   shape_name     = "VM.Standard.E2.1.Micro"
+#   image_id       = var.image_ocid
+
+# }
+
+data "oci_core_image" "ubuntu" {
+  #Required
+  image_id = var.image_ocid
 }
 
-resource "oci_core_shape_management" "amd64" {
-  # availability_domain = data.oci_identity_availability_domain.ad1.name
-  compartment_id = var.compartment_ocid
-  shape_name     = "VM.Standard.E2.1.Micro"
-  image_id       = var.image_ocid
-
+data "oci_core_image_shape" "arm" {
+  #Required
+  image_id   = oci_core_image.ubuntu.id
+  shape_name = "VM.Standard.A1.Flex"
 }
 
 data "oci_identity_availability_domain" "ad2" {
