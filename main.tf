dd966f3287a1062c69596cf13545447e7d7e5278
Artur Angiel <arturangiel@gmail.com>
2022-09-28 14:52:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ data "oci_identity_availability_domain" "ad2" {
 
 
 resource "oci_core_instance" "arm" {
-  availability_domain = data.oci_identity_availability_domain.ad2
+  availability_domain = data.oci_identity_availability_domain.ad2.name
   compartment_id      = var.compartment_ocid
   shape               = data.oci_core_image_shape.arm.id
 
