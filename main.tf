3b7cd7fcb1dae035872f7f99d944b3ae48c77358
Artur Angiel <arturangiel@gmail.com>
2022-09-29 18:15:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "oci_core_instance" "arm" {
   compartment_id      = var.compartment_ocid
   shape               = var.arm_shape_name
   count               = 2
-  display_name = "hashi"
+  display_name = "hashi-${count.index}"
 
   source_details {
     source_type = "image"
@@ -96,7 +96,7 @@ resource "oci_core_instance" "arm" {
 
   create_vnic_details {
     subnet_id = oci_core_subnet.hashistack.id
-    hostname_label = "hashi"
+    hostname_label = "hashi-${count.index}"
   }
 
   shape_config {
@@ -122,7 +122,7 @@ resource "oci_core_instance" "amd" {
   compartment_id      = var.compartment_ocid
   shape               = var.amd_shape_name
   count               = 2
-  display_name = "hashi"
+  display_name = "hashi-${count.index}"
 
   source_details {
     source_type = "image"
@@ -131,7 +131,7 @@ resource "oci_core_instance" "amd" {
 
   create_vnic_details {
     subnet_id = oci_core_subnet.hashistack.id
-    hostname_label = "hashi"
+    hostname_label = "hashi-${count.index}"
   }
 
   metadata = {
