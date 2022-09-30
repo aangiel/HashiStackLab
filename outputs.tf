57d6a08004fa3223fbeafd5093ae1ddf47e7c22f
Artur Angiel <arturangiel@gmail.com>
2022-09-30 11:51:43 +0200

    default message

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vcn_id" {
 output "vcn_attributes" {
   value = module.vcn.vcn_all_attributes
 }
+
+output "subnet_id" {
+  value = module.vcn.subnet_id
+}
+
+output "subnet_all_attrs" {
+  value = module.vcn.subnet_all_attributes
+}
