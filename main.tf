b1a34a1abb36a03c5cd025d8e035824b0450aa66
Artur Angiel <arturangiel@gmail.com>
2022-09-30 20:37:31 +0200

    default message

--- main.tf
+++ main.tf
@@ -27,9 +27,12 @@ module "vcn_subnet" {
 
   subnets = {
     subnet = {
-      cidr_block        = "10.0.1.0/24"
-      dns_label         = "subnet"
-      security_list_ids = [module.vcn.vcn_all_attributes.default_security_list_id, oci_core_security_list.hashistack_sec_list.id]
+      cidr_block = "10.0.1.0/24"
+      dns_label  = "subnet"
+      security_list_ids = [
+        module.vcn.vcn_all_attributes.default_security_list_id,
+        oci_core_security_list.hashistack_sec_list.id
+      ]
     }
   }
 }
@@ -49,7 +52,7 @@ resource "oci_core_security_list" "hashistack_sec_list" {
   }
 }
 
-module "compute-instance" {
+module "arm-compute-instance-1" {
   source  = "oracle-terraform-modules/compute-instance/oci"
   version = ">=2.4.0"
 
@@ -70,8 +73,102 @@ module "compute-instance" {
   instance_count              = 1
   instance_display_name       = "hashi-arm"
   shape                       = "VM.Standard.A1.Flex"
-  user_data                   = base64encode(templatefile("./user-data.tftpl", {}))
-  public_ip                   = "EPHEMERAL"
+  user_data = base64encode(templatefile("./user-data.tftpl", {
+    vault_leader_instance   = true
+    vault_follower_instance = false
+    vault_transit_instance  = false
+  }))
+  public_ip = "EPHEMERAL"
+
+}
+
+module "arm-compute-instance-2" {
+  source  = "oracle-terraform-modules/compute-instance/oci"
+  version = ">=2.4.0"
+
+  ad_number                   = 2
+  boot_volume_size_in_gbs     = 50
+  compartment_ocid            = var.compartment_ocid
+  defined_tags                = {}
+  freeform_tags               = {}
+  instance_flex_memory_in_gbs = 12
+  instance_flex_ocpus         = 2
+  ipxe_script                 = ""
+  primary_vnic_nsg_ids        = []
+  public_ip_display_name      = "arm"
+  source_ocid                 = var.image_ocid
+  ssh_public_keys             = file("./public-keys")
+  subnet_ocids                = values(module.vcn_subnet.subnet_id)
+  hostname_label              = "hashi-arm"
+  instance_count              = 1
+  instance_display_name       = "hashi-arm"
+  shape                       = "VM.Standard.A1.Flex"
+  user_data = base64encode(templatefile("./user-data.tftpl", {
+    vault_leader_instance   = false
+    vault_follower_instance = true
+    vault_transit_instance  = false
+  }))
+  public_ip = "EPHEMERAL"
+
+}
+
+module "amd-compute-instance-1" {
+  source  = "oracle-terraform-modules/compute-instance/oci"
+  version = ">=2.4.0"
+
+  ad_number                   = 1
+  boot_volume_size_in_gbs     = 50
+  compartment_ocid            = var.compartment_ocid
+  defined_tags                = {}
+  freeform_tags               = {}
+  instance_flex_memory_in_gbs = 12
+  instance_flex_ocpus         = 2
+  ipxe_script                 = ""
+  primary_vnic_nsg_ids        = []
+  public_ip_display_name      = "hashi-amd-1"
+  source_ocid                 = var.image_ocid
+  ssh_public_keys             = file("./public-keys")
+  subnet_ocids                = values(module.vcn_subnet.subnet_id)
+  hostname_label              = "hashi-amd-1"
+  instance_count              = 1
+  instance_display_name       = "hashi-amd-1"
+  shape                       = "VM.Standard.E2.Micro"
+  user_data = base64encode(templatefile("./user-data.tftpl", {
+    vault_leader_instance   = false
+    vault_follower_instance = false
+    vault_transit_instance  = true
+  }))
+  public_ip = "EPHEMERAL"
+
+}
+
+module "amd-compute-instance-2" {
+  source  = "oracle-terraform-modules/compute-instance/oci"
+  version = ">=2.4.0"
+
+  ad_number                   = 1
+  boot_volume_size_in_gbs     = 50
+  compartment_ocid            = var.compartment_ocid
+  defined_tags                = {}
+  freeform_tags               = {}
+  instance_flex_memory_in_gbs = 12
+  instance_flex_ocpus         = 2
+  ipxe_script                 = ""
+  primary_vnic_nsg_ids        = []
+  public_ip_display_name      = "hashi-amd-2"
+  source_ocid                 = var.image_ocid
+  ssh_public_keys             = file("./public-keys")
+  subnet_ocids                = values(module.vcn_subnet.subnet_id)
+  hostname_label              = "hashi-amd-2"
+  instance_count              = 1
+  instance_display_name       = "hashi-amd-2"
+  shape                       = "VM.Standard.E2.Micro"
+  user_data = base64encode(templatefile("./user-data.tftpl", {
+    vault_leader_instance   = false
+    vault_follower_instance = true
+    vault_transit_instance  = false
+  }))
+  public_ip = "EPHEMERAL"
 
 }
 
